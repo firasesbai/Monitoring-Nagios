c7accf9d29a32027192b3701b8a8e93fc8f5e501
firasesbai <firasesbai@gmail.com>
2017-12-09 09:36:36 +0100

    Update install-nagios.sh

--- installing/install-nagios.sh
+++ installing/install-nagios.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+#Prerequisites: A LAMP (Linux, Apache , MySQL, PHP) stack is required for the Installation of Nagios Core
+#Install Apache web server and allow it in firewall 
+sudo apt-get install apache2
+sudo ufw allow in "Apache Full"
+#Install MySQL 
+sudo apt-get install mysql-server
+#Install PHP
+sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql
+
+#Install Nagios 4
 
