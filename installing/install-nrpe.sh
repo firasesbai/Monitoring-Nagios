845d376bc27739960db9207d1175e57b8b3eb227
firasesbai <firasesbai@gmail.com>
2017-12-09 08:52:52 +0100

    Update install-nrpe.sh

--- installing/install-nrpe.sh
+++ installing/install-nrpe.sh
@@ -1,1 +1,11 @@
-
+#!/bin/bash
+# Download the latest version of NRPE and extract its content
+wget http://prdownloads.sourceforge.net/sourceforge/nagios/nrpe-2.15.tar.gz
+tar -xzf nrpe.2.15.tar.gz
+cd nrpe.2.15
+#Configure NRPE
+sudo ./configure
+#Install NRPE
+sudo make all
+sudo make install-daemon 
+sudo make install-daemon-config
